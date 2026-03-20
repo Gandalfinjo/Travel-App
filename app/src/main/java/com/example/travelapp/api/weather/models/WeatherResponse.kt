c9d4f3e27058d5973bd1f493c9d74662c36ca163
Dragan Peric <draganceperic@gmail.com>
2026-03-20 12:49:12 +0100

    Added javadoc comments for WeatherResponse

--- app/src/main/java/com/example/travelapp/api/weather/models/WeatherResponse.kt
+++ app/src/main/java/com/example/travelapp/api/weather/models/WeatherResponse.kt
@@ -1,5 +1,9 @@
 package com.example.travelapp.api.weather.models
 
+/**
+ * Weather data response from OpenWeatherMap API.
+ * Temperatures are in Celsius, wind speed in m/s.
+ */
 data class WeatherResponse(
     val name: String,
     val main: Main,
