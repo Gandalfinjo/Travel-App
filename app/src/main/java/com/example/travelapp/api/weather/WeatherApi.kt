96cb7838dc6d24f667ef76f2867aee6338bae06a
Dragan Peric <draganceperic@gmail.com>
2026-03-29 17:28:26 +0200

    Hid the weather api key

--- app/src/main/java/com/example/travelapp/api/weather/WeatherApi.kt
+++ app/src/main/java/com/example/travelapp/api/weather/WeatherApi.kt
@@ -1,10 +1,12 @@
 package com.example.travelapp.api.weather
 
+import com.example.travelapp.BuildConfig
 import com.example.travelapp.api.weather.models.WeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 const val BASE_URL = "https://api.openweathermap.org/"
+const val WEATHER_API_KEY = BuildConfig.WEATHER_API_KEY
 
 /**
  * Retrofit API interface for OpenWeatherMap API.
@@ -24,7 +26,7 @@ interface WeatherApi {
     @GET("data/2.5/weather")
     suspend fun gerCurrentWeather(
         @Query("q") city: String,
-        @Query("appid") apiKey: String = "284357caeb44a80d573b10cc99b69b13",
+        @Query("appid") apiKey: String = WEATHER_API_KEY,
         @Query("units") units: String = "metric",
         @Query("lang") lang: String = "en"
     ): WeatherResponse
